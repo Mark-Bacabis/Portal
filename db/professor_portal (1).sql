c035a7726668df796581389c71cec1cfaf6d63ad
JessicaBulleque <84769388+JessicaBulleque@users.noreply.github.com>
2021-12-03 15:36:57 +0800

    db updated

--- db/professor_portal (1).sql
+++ db/professor_portal (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2021 at 04:35 PM
+-- Generation Time: Dec 03, 2021 at 08:35 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
